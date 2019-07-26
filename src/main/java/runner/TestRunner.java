33e047a4e7ada2c042b1281021e2f4274800da47
Gokul-Shanmugam <gokulns94@gmail.com>
2019-07-26 08:39:58 +0530

    Updated in 26/7
    Signed-off-by: Gokul-Shanmugam <gokulns94@gmail.com>

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -13,33 +13,12 @@ import cucumber.api.testng.TestNGCucumberRunner;
 
 @CucumberOptions(
 		format = {"pretty","html:report/html","json:report/json/cucumber.json"},
-		//tags={"@tst"},
+		tags={"@SIT`"},
 		features = "src/main/java/features",
 		glue={"stepDefinitions"}
  )
 
 public class TestRunner extends AbstractTestNGCucumberTests{
-//	
-//	private TestNGCucumberRunner testNGCucumberRunner;
-//	 
-//    @BeforeClass(alwaysRun = true)
-//    public void setUpClass() throws Exception {
-//        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-//    }
-// 
-//    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
-//    public void feature(CucumberFeatureWrapper cucumberFeature) {
-//        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-//    }
-// 
-//    @DataProvider
-//    public Object[][] features() {
-//        return testNGCucumberRunner.provideFeatures();
-//    }
-// 
-//    @AfterClass(alwaysRun = true)
-//    public void tearDownClass() throws Exception {
-//        testNGCucumberRunner.finish();
-//    }
+
 	
 }
--- src/main/java/stepDefinitions/LoginStepDefinition.java
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -1,11 +1,18 @@
 package stepDefinitions;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 
 import cucumber.api.java.en.Given;
@@ -17,12 +24,21 @@ public class LoginStepDefinition{
 
 	  WebDriver driver;
 
+	  @Given("^user is on Login Page with url \"(.*)\"$")
+	  public void login(String url){
+		  
+	  }
+
 	
 	 @Given("^user is on Login Page$")
-	 public void user_already_on_login_page(){
+	 public void user_already_on_login_page() throws MalformedURLException{
 	 System.setProperty("webdriver.chrome.driver","C:\\Users\\Gokul\\Desktop\\Selenium\\Jar\\chromedriver.exe");
-	 driver = new ChromeDriver();
-	 driver.get("https://www.freecrm.com/index.html");
+	 //DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+	 ChromeOptions option=new ChromeOptions();
+	// option.setHeadless(true);
+//	 driver = new RemoteWebDriver(new URL("http://192.168.1.33:4444/wd/hub"), capabilities);
+	 driver = new ChromeDriver(option);
+	 driver.get("https://classic.crmpro.com/login.cfm");
 	 }
 	
 	
@@ -45,10 +61,16 @@ public class LoginStepDefinition{
 	
 	 @Then("^user clicks on login button$")
 	 public void user_clicks_on_login_button() {
-	 WebElement loginBtn =
-	 driver.findElement(By.xpath("//input[@type='submit']"));
+	 WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
 	 JavascriptExecutor js = (JavascriptExecutor)driver;
+	 
+	 
 	 js.executeScript("arguments[0].click();", loginBtn);
+	 
+	//Vertical scroll down by 600  pixels		
+//     js.executeScript("window.scrollBy(0,600)");		
+//     
+//     js.executeScript("document.getElementById('elementID').setAttribute('value', 'Gokul')");
 	 }
 	
 	
@@ -56,7 +78,7 @@ public class LoginStepDefinition{
 	 public void user_is_on_hopme_page(){
 	 String title = driver.getTitle();
 	 System.out.println("Home Page title ::"+ title);
-	 Assert.assertEquals("CRMPRO", title);
+	 Assert.assertEquals("CRMPRO - CRM software for customer relationship management, sales, and support.", title);
 	 }
 	 
 	 @Then("^user moves to new contact page$")
